3b0c76128d613642d819098ff8fd448746a670df
Ancherm <andrewcherm2014@gmail.com>
2024-10-26 23:07:52 +0400

    add database with ORM

--- sibentek-restful/Program.cs
+++ sibentek-restful/Program.cs
@@ -1,3 +1,5 @@
+using Sibentek.Core.Model;
+using Sibentek.DataAccess;
 using sibentek_restful;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,29 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 var bot = new TelegramBot("7410806777:AAGVoLLKhd5eqQ13nYCwWjY5o_t6fYlV9nY");
+
+using (var context = new ApplicationDbContext())
+{
+    context.Database.EnsureCreated();
+    
+    // Создание нового пользователя
+    User user = new User {Name = "Ancherm"};
+    context.Users.Add(user);
+    context.SaveChanges();
+    
+    // Чтение пользователей
+    var users = context.Users.ToList();
+    Console.WriteLine(string.Join(", ", users));
+
+    // Обновление пользователя
+    var firstUser = context.Users.First();
+    firstUser.Name = "Bob";
+    context.SaveChanges();
+    
+    // Удаление пользователя
+    context.Users.Remove(firstUser);
+    context.SaveChanges();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
