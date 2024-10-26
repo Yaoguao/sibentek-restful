57f48ecbc08a20e8b15c6e6a2569a5781e92ce49
Yaoguao <126403423+Yaoguao@users.noreply.github.com>
2024-10-26 19:40:18 +0400

    Create project

---
+++ sibentek-restful/Controllers/WeatherForecastController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace sibentek_restful.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    
+}
