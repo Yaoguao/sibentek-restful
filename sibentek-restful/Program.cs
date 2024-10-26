d86d9cae2fa3f2f2ae915d8bfce659c54962b68b
Yaoguao <126403423+Yaoguao@users.noreply.github.com>
2024-10-26 21:36:09 +0400

    Add in program log tmp

--- sibentek-restful/Program.cs
+++ sibentek-restful/Program.cs
@@ -1,3 +1,4 @@
+using ModelTextForApi;
 using sibentek_restful;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Logging
+// TODO - убрать как закончим
+var sampleData = new MLModel1.ModelInput()
+{
+    Topic = @"I cant connect internet",
+};
+var res = MLModel1.Predict(sampleData);
+
+Console.WriteLine("Log ml: " + res.PredictedLabel);
+Console.WriteLine("Log ml: " + res.Score[0] * 100);
+// log
 
 var app = builder.Build();
 var bot = new TelegramBot("7410806777:AAGVoLLKhd5eqQ13nYCwWjY5o_t6fYlV9nY");
